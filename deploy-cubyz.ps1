c9a1b4a1bbacf652d87fd87a78bd9f6eca1c5703
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-12-20 14:26:26 +0000

    Fully working

--- deploy-cubyz.ps1
+++ deploy-cubyz.ps1
@@ -1,6 +1,16 @@
+$currentTime = Get-Date -Format "MMddyyyy-HHmm"
+
+mkdir target
 git clone https://github.com/PixelGuys/Cubyz.git
 Remove-Item -Force "./Cubyz/pom.xml"
 Copy-Item -Path "./pom.xml" -Destination "./Cubyz/pom.xml"
 cd "./Cubyz"
+(Get-Content "./pom.xml").replace('replace_this', "$currentTime") | Set-Content "./pom.xml"
 mvn clean deploy
 cd ..
+Remove-Item -Recurse -Force "./Cubyz"
+rm target
+git add maven/*
+git commit -m "Deploy $currentTime"
+git pull
+git push
