3ec48c99e8dfd6c90259411438d6dfde053e68f4
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-12-20 19:03:06 +0000

    Done some fixes.
    Fix the fact that the jar were empty lol
    Fix the readme file not being generated correctly

--- deploy-cubyz.ps1
+++ deploy-cubyz.ps1
@@ -7,12 +7,12 @@ Remove-Item -Force "./Cubyz/pom.xml"
 Copy-Item -Path "./pom.xml" -Destination "./Cubyz/pom.xml"
 cd "./Cubyz"
 (Get-Content "./pom.xml").replace('replace_this', "$currentTime") | Set-Content "./pom.xml"
-mvn clean deploy
+mvn clean package deploy
 cd ..
 Remove-Item -Recurse -Force "./Cubyz"
 rm target
 cd maven
-Add-Content "./readme.md" "-$currentTime : $formattedTime `n"
+Add-Content "./readme.md" "- $currentTime : $formattedTime `n"
 cd ..
 git add maven/*
 git commit -m "Deploy $currentTime"
